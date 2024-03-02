5bcb6140f0ceaac3d7fb49eb2ceb60f246939b89
Irfan Gondal <irfangondal60@gmail.com>
2024-03-01 20:37:50 -0800

    Git Commands file updated after 'Get started' Chapter

---
+++ Git Commands.php
@@ -0,0 +1,14 @@
+git --version				//to check git version or checking that git is working successfully
+
+git config --global user.name "w3schools-test" //to set git username globally
+git config --global user.email "test@w3schools.com"	// to set git email globally
+Note: Use global to set the username and e-mail for every repository on your computer.
+If you want to set the username/e-mail for just the current repo, you can remove global
+
+mkdir myproject	//to create a new folder/directory
+				//mkdir makes a new directory.
+cd myproject	//to go to myproject folder
+				//cd changes the current working directory.
+				
+git init 		//to initalize empty git repository
+
