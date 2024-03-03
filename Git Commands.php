18b2f04fc807b1f7f076f2ba6ce15ed45a5d11ce
Irfan Gondal <irfangondal60@gmail.com>
2024-03-03 00:33:08 -0800

    Made some changes text format for good looking

--- Git Commands.php
+++ Git Commands.php
@@ -1,65 +1,98 @@
-git --version				//to check git version or checking that git is working successfully
-
-git config --global user.name "w3schools-test" //to set git username globally
-git config --global user.email "test@w3schools.com"	// to set git email globally
-Note: Use global to set the username and e-mail for every repository on your computer.
-If you want to set the username/e-mail for just the current repo, you can remove global
-
-mkdir myproject	//to create a new folder/directory
-				//mkdir makes a new directory.
-cd myproject	//to go to myproject folder
-				//cd changes the current working directory.
-				
-git init 		//to initalize empty git repository
-ls				//list of all files
-git status		//to check the status of current repository
-
-git add index.html	//to add index.html to stagaging area
-git add --all		//to stage all files in pwd
-
-Note: The shorthand command for git add --all is git add -A
-
-git status --short		//to check status with short/compact version 
-Note: Short status flags are:
-	?? - Untracked files
-	A - Files added to stage
-	M - Modified files
-	D - Deleted files
-
-git commit -a -m "Message about commit"	//to commit without staging area
-
-Warning: Skipping the Staging Environment is not generally recommended.
-Skipping the stage step can sometimes make you include unwanted changes.
-
-git log 		//to view commits history/ log
-press q to exit
-
-git command -help  //to See all the available options for the specific command
-git help --all  //to See all possible commands
-
-Note: You can also use --help instead of -help to open the relevant Git manual page
-
-git help --all  //to See all possible commands
-Warning: This will display a very long list of commands
-Note: If you find yourself stuck in the list view, SHIFT + G to jump the end of the list, then q to exit the view.
-
-git branch hello-world //to create a new branch name of 'hello-world', 
-						//with the copy of same code where you create a new branch
-						
-git branch				//to see the list of all branch in the repository
-
-We can see the new branch with the name "hello-world", but the * beside master specifies that we are currently on that branch.
-
-checkout is the command used to check out a branch. Moving us from the current branch, to the one specified at the end of the command:
-
-git checkout hello-world	//Switched to branch 'hello-world'
-
-Note: Using the -b option on checkout will create a new branch, and move to it, if it does not exist
-
-
-git merge branchName 		//to merge branchName into master branch
-							//this command should be run from master branch
+git --version
+	to check git version or checking that git is working successfully
 	
-	git branch -d branchName	//to delete branch "branchName"
+git config --global user.name "Irfan Gondal" 
+	to set git username globally
+	
+git config --global user.email "irfan@gmail.com"	
+	to set git email globally
+	
+	Note: Use global to set the username and e-mail for every 
+	repository on your computer. If you want to set the username/ 
+	e-mail for just the current repo, you can remove --global.
+
+mkdir myproject	
+	to create a new folder/directory
+	mkdir makes a new directory.
+
+cd myproject	
+	to go to myproject folder
+	cd changes the current working directory.
+				
+git init 		
+	to initalize empty git repository
+
+ls
+	list of all files
+
+git status
+	to check the status of current repository
+
+git add index.html
+	to add index.html to stagaging area
+
+git add --all
+	to stage all files in pwd
+	Note: The shorthand command for "git add --all" is "git add -A"
+
+git status --short
+	to check status with short/compact version 
+	
+	Note: Short status flags are:
+		?? - Untracked files
+		A - Files added to stage
+		M - Modified files
+		D - Deleted files
+
+git commit -a -m "Message about commit"
+	to commit without staging area
+
+	Warning: Skipping the Staging Environment is not generally 
+	recommended. Skipping the stage step can sometimes make you 
+	include unwanted changes.
+
+git log
+	to view commits history/ log
+	SHIFT + G to jump the end of the list, then q to exit the view.
+
+git command -help
+	to See all the available options for the specific command
+
+git help --all
+	to See all possible commands
+	Note: You can also use --help instead of -help to open the 
+	relevant Git manual page
+	
+	Warning: This will display a very long list of commands
+	Note: If you find yourself stuck in the list view, 
+	SHIFT + G to jump the end of the list, then q to exit the view.
+
+git branch hello-world
+	to create a new branch name of 'hello-world', 
+	with the copy of same code where you create a new branch
+						
+git branch
+	to see the list of all branch in the repository
+
+	We can see the new branch with the name "hello-world",
+	but the * beside master specifies that we are currently 
+	on that branch.
+
+	checkout is the command used to check out a branch. Moving us 
+	from the current branch, to the one specified at the end of 
+	the command:
+
+git checkout hello-world
+	Switched to branch 'hello-world'
+	Note: Using the -b option on checkout will create a new branch, 
+	and move to it, if it does not exist
+
+
+git merge branchName
+	to merge branchName into master branch
+	this command should be run from master branch
+	
+git branch -d branchName
+	to delete branch "branchName"
 	
 	
--- Learn Theory Git.php
+++ Learn Theory Git.php
@@ -2,19 +2,23 @@ Learn Git
 
 What is Git?
 
-Git is a popular version control system. It was created by Linus Torvalds in 2005, and has been maintained by Junio Hamano since then.
-It is used for:
-*Tracking code changes
-*Tracking who made changes
-*Coding collaboration
+	Git is a popular version control system. It was created by 
+	Linus Torvalds in 2005, and has been maintained by Junio Hamano 
+	since then. 
+
+	It is used for:
+		Tracking code changes
+		Tracking who made changes
+		Coding collaboration
 
 What does Git do?
-*Manage projects with Repositories
-*Clone a project to work on a local copy
-*Control and track changes with Staging and Committing
-*Branch and Merge to allow for work on different parts and versions of a project
-*Pull the latest version of the project to a local copy
-*Push local updates to the main project
+	*Manage projects with Repositories
+	*Clone a project to work on a local copy
+	*Control and track changes with Staging and Committing
+	*Branch and Merge to allow for work on different parts 
+	and versions of a project
+	*Pull the latest version of the project to a local copy
+	*Push local updates to the main project
 
 Working with Git
 	Initialize Git on a folder, making it a Repository
@@ -39,40 +43,60 @@ What is GitHub?
 	In this tutorial, we will focus on using Git with GitHub.
 	
 Git status
-Now Git is aware of the file, but has not added it to our repository!
-Files in your Git repository folder can be in one of 2 states:
-	Tracked - files that Git knows about and are added to the repository
-	Untracked - files that are in your working directory, but not added to the repository
+	Now Git is aware of the file, but has not added it to our 
+	repository!
+	Files in your Git repository folder can be in one of 2 states:
+		Tracked - files that Git knows about and are added to the 
+		repository
+		Untracked - files that are in your working directory, but 
+		not added to the repository
 
 git add --all
-	Using --all instead of individual filenames will stage all changes (new, modified, and deleted) files.
+	Using --all instead of individual filenames will stage all 
+	changes (new, modified, and deleted) files.
 
 Git Commit
-Since we have finished our work, we are ready move from stage to commit for our repo.
-Adding commits keep track of our progress and changes as we work. Git considers each commit change point or "save point". It is a point in the project you can go back to if you find a bug, or want to make a change.
-When we commit, we should always include a message.
-By adding clear messages to each commit, it is easy for yourself (and others) to see what has changed and when.
-The commit command performs a commit, and the -m "message" adds a message.
+	Since we have finished our work, we are ready move from stage to 
+	commit for our repo.
+	Adding commits keep track of our progress and changes as we work. 
+	Git considers each commit change point or "save point". 
+	It is a point in the project you can go back to if you find a 
+	bug, or want to make a change.
+	When we commit, we should always include a message.
+	By adding clear messages to each commit, it is easy for 
+	yourself (and others) to see what has changed and when.
+	The commit command performs a commit, and the -m "message" 
+	adds a message.
 
 Git Commit without Stage
-Sometimes, when you make small changes, using the staging environment seems like a waste of time. It is possible to commit changes directly, skipping the staging environment. The -a option will automatically stage every changed, already tracked file.
+	Sometimes, when you make small changes, using the staging 
+	environment seems like a waste of time. It is possible to 
+	commit changes directly, skipping the staging environment. 
+	The -a option will automatically stage every changed, 
+	already tracked file.
 
 Working with Git Branches
-In Git, a branch is a new/separate version of the main repository.
-Let's say you have a large project, and you need to update the design on it.
-Branches allow you to work on different parts of a project without impacting the main branch.
-When the work is complete, a branch can be merged with the main project.
-You can even switch between branches and work on different projects without them interfering with each other.
-Branching in Git is very lightweight and fast!
+	In Git, a branch is a new/separate version of the main 
+	repository.
+	Let's say you have a large project, and you need to update the 
+	design on it.
+	Branches allow you to work on different parts of a project 
+	without impacting the main branch.
+	When the work is complete, a branch can be merged with the main 
+	project.
+	You can even switch between branches and work on different 
+	projects without them interfering with each other.
+	Branching in Git is very lightweight and fast!
 
 Merge Branches
-to merge a branch first all changes to be commited before merging.
-Then checkout/switch to master branch and run this Command:
-git merge branchName
+	to merge a branch first all changes to be commited before 
+	merging.
+	Then checkout/switch to master branch and run this Command:
+		git merge branchName
 
 Merge Conflict
-Git can handle most merges on its own with automatic merging 
-features. A conflict arises when two separate branches have made 
-edits to the same line in a file, or when a file has been deleted 
-in one branch but edited in the other. Conflicts will most likely 
-happen when working in a team environment.
+	Git can handle most merges on its own with automatic merging 
+	features. A conflict arises when two separate branches have 
+	made edits to the same line in a file, or when a file has been 
+	deleted in one branch but edited in the other. Conflicts will 
+	most likely happen when working in a team environment.
